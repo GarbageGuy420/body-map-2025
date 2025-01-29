a9fb98964dbca0a84f38f5c12f9829caf7426b55
GarbageGuy420 <158623079+GarbageGuy420@users.noreply.github.com>
2025-01-29 16:50:09 -0500

    Update anatomy-config.js

--- anatomy-config.js
+++ anatomy-config.js
@@ -1,48 +1,56 @@
 var basic_config = {
-    "basic_1":{//head
-        "hover": "HEAD",//info of the popup
-        "url": "https://www.humananatomyillustrations.com/",//link to any webpage
-        "target": "_self",// use "_blank", "_self", or "none"
-        "active": true//true/false to show/hide
+    "basic_1": {//head
+        "hover": "HEAD",
+        "url": "https://www.humananatomyillustrations.com/",
+        "target": "_self",
+        "active": false
     },
-    "basic_2":{//neck
+    "basic_2": {//neck
         "hover": "NECK",
-        "url": "https://www.humananatomyillustrations.com/", "target": "_self",
-        "active": true
+        "url": "https://www.humananatomyillustrations.com/",
+        "target": "_self",
+        "active": false
     },
-    "basic_3":{//chest
+    "basic_3": {//chest
         "hover": "CHEST",
-        "url": "https://www.humananatomyillustrations.com/", "target": "_self",
-        "active": true
+        "url": "https://www.humananatomyillustrations.com/",
+        "target": "_self",
+        "active": false
     },
-    "basic_4":{//abdomen
+    "basic_4": {//abdomen
         "hover": "ABDOMEN",
-        "url": "https://www.humananatomyillustrations.com/", "target": "_self",
-        "active": true
+        "url": "https://www.humananatomyillustrations.com/",
+        "target": "_self",
+        "active": false
     },
-    "basic_5":{//pelvis
+    "basic_5": {//pelvis
         "hover": "PELVIS",
-        "url": "https://www.humananatomyillustrations.com/", "target": "_self",
-        "active": true
+        "url": "https://www.humananatomyillustrations.com/",
+        "target": "_self",
+        "active": false
     },
-    "basic_6":{//arm-rt
+    "basic_6": {//arm-rt
         "hover": "ARM [RT]",
-        "url": "https://www.humananatomyillustrations.com/", "target": "_self",
-        "active": true
+        "url": "https://www.humananatomyillustrations.com/",
+        "target": "_self",
+        "active": false
     },
-    "basic_7":{//arm-lt
+    "basic_7": {//arm-lt
         "hover": "ARM [LT]",
-        "url": "https://www.humananatomyillustrations.com/", "target": "_self",
-        "active": true
+        "url": "https://www.humananatomyillustrations.com/",
+        "target": "_self",
+        "active": false
     },
-    "basic_8":{//leg-rt
+    "basic_8": {//leg-rt
         "hover": "LEG [RT]",
-        "url": "https://www.humananatomyillustrations.com/", "target": "_self",
-        "active": true
+        "url": "https://www.humananatomyillustrations.com/",
+        "target": "_self",
+        "active": false
     },
-    "basic_9":{//leg-lt
+    "basic_9": {//leg-lt
         "hover": "LEG [LT]",
-        "url": "https://www.humananatomyillustrations.com/", "target": "_self",
-        "active": true
+        "url": "https://www.humananatomyillustrations.com/",
+        "target": "_self",
+        "active": false
     }
 };
