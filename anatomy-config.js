0ce0761832454a379082dfc4647f2f4720bca0d7
GarbageGuy420 <158623079+GarbageGuy420@users.noreply.github.com>
2025-01-29 21:57:09 -0500

    Update anatomy-config.js

--- anatomy-config.js
+++ anatomy-config.js
@@ -7,42 +7,42 @@ var basic_config = {
     },
     "basic_2":{//neck
         "hover": "NECK",
-        "url": "https://www.humananatomyillustrations.com/", "target": "none",
+        "url": "none", "target": "none",
         "active": true
     },
     "basic_3":{//chest
         "hover": "CHEST",
-        "url": "https://www.humananatomyillustrations.com/", "target": "none",
+        "url": "none", "target": "none",
         "active": true
     },
     "basic_4":{//abdomen
         "hover": "ABDOMEN",
-        "url": "https://www.humananatomyillustrations.com/", "target": "none",
+        "url": "none", "target": "none",
         "active": true
     },
     "basic_5":{//pelvis
         "hover": "PELVIS",
-        "url": "https://www.humananatomyillustrations.com/", "target": "none",
+        "url": "none", "target": "none",
         "active": true
     },
     "basic_6":{//arm-rt
         "hover": "ARM [RT]",
-        "url": "https://www.humananatomyillustrations.com/", "target": "none",
+        "url": "none", "target": "none",
         "active": true
     },
     "basic_7":{//arm-lt
         "hover": "ARM [LT]",
-        "url": "https://www.humananatomyillustrations.com/", "target": "none",
+        "url": "none", "target": "none",
         "active": true
     },
     "basic_8":{//leg-rt
         "hover": "LEG [RT]",
-        "url": "https://www.humananatomyillustrations.com/", "target": "none",
+        "url": "none", "target": "none",
         "active": true
     },
     "basic_9":{//leg-lt
         "hover": "LEG [LT]",
-        "url": "https://www.humananatomyillustrations.com/", "target": "none",
+        "url": "none", "target": "none",
         "active": true
     }
 };
